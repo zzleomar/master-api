7c3826539122568a3bf19c078d2fb08d75ebdb5b
Rafael Emilio Guzman <regf.12@gmail.com>
2023-12-29 20:16:29 +0000

    fix: edicion de marca, modelo a~o del vehiculo

--- src/modules/budgets/budgets.controller.ts
+++ src/modules/budgets/budgets.controller.ts
@@ -240,8 +240,6 @@ export class BudgetsController {
       updateBudgetDto,
     );
 
-    console.log('updateBudgetDto: ', updateBudgetDto);
-
     const order = await this.repairOrdersService.findBy(
       {
         budget: newBudget._id,
@@ -291,6 +289,7 @@ export class BudgetsController {
         });
         newBudget.history.push(log1.id);
       }
+
       if (
         updateBudgetDto.insuranceCompany != dataBudget.insuranceCompany._id ||
         updateBudgetDto.claimNumber != dataBudget.claimNumber ||
